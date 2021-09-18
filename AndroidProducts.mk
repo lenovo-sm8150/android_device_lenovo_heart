f170748654263a436186429b37db287ecc457828
Einar Gednochsson <pete80pro.pb@gmail.com>
2021-09-18 16:52:34 +0200

    sm8150-common: derpfest: Changes for Heart

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_heart.mk
+    $(LOCAL_DIR)/derp_heart.mk
--- derp_heart.mk
+++ derp_heart.mk
@@ -6,15 +6,15 @@
 
 $(call inherit-product, device/lenovo/heart/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Derpfest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := heart
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo L78032
-PRODUCT_NAME := lineage_heart
+PRODUCT_NAME := derp_heart
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
